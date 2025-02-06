839411f0ef59b47235fedc5b5476dce554d52ba6
Islam Ali <islamali3020@gmail.com>
2025-02-06 20:33:43 +0200

    create best seller item part one

--- lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -6,16 +6,16 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(aspectRatio: 2.7/4,
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
       child: Container(
-        height: MediaQuery.of(context).size.height *.25,
-      width: 100,
-        decoration:  BoxDecoration(borderRadius:BorderRadius.circular(16) ,
-      color: Colors.blue,
-      
-          image: const DecorationImage(fit: BoxFit.fill,
-            image: AssetImage(AssetsData.testImage))
-        ),
+        height: MediaQuery.of(context).size.height * .25,
+        width: 100,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.blue,
+            image: const DecorationImage(
+                fit: BoxFit.fill, image: AssetImage(AssetsData.testImage))),
       ),
     );
   }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,4 @@
-
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view.dart';
@@ -11,13 +11,45 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 24.0),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-        children: [CustomAppBar(),
-        CustomListView(),
-         SizedBox(height: 50,),
-        Text('Best Seller',style: Styles.titleMedium,)
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          CustomListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          ),
+          BestSellerListViewItem(),
         ],
       ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: 125,
+      child: Row(children: [
+        AspectRatio(
+        aspectRatio: 2.5 / 4,
+        child: Container(
+          height: MediaQuery.of(context).size.height * .25,
+          width: 100,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.blue,
+              image: const DecorationImage(
+                  fit: BoxFit.fill, image: AssetImage(AssetsData.testImage))),
+        ),
+      ),
+      ],),
     );
   }
 }
